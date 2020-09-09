30d11ba42d343738b371db18140488d3aa35d877
Tamara Zard <tamara.zardaboujaoudeh@mail.mcgill.ca>
2020-09-09 21:26:16 +0300

    Trial

--- TestGit/src/test/git/Student.java
+++ TestGit/src/test/git/Student.java
@@ -1,5 +1,16 @@
 package test.git;
-
 public class Student {
 
+		String name;
+		
+		public Student() {	
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}	
 }
